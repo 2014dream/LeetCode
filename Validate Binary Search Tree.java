70f710f06e55c5bc71b35565c6ccf314dc231c2d
2014dream <stevencraglist@gmail.com>
2014-02-27 11:47:54 -0700

    Update Validate Binary Search Tree.java

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -28,3 +28,50 @@ public class Solution {
             return Helper(root.left,minVal,root.val) && Helper(root.right,root.val, maxVal);
     }
 }
+
+
+/* Follow up: Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3 */
+   
+/* It is similar to the above solution, find out the left and right subtrees, then use recursive way to construct. */
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; left = null; right = null; }
+ * }
+ */
+public class Solution {
+    public ArrayList<TreeNode> generateTrees(int n) {
+        return generateTrees(1,n);
+    }
+    public ArrayList<TreeNode> generateTrees(int start, int end){
+        ArrayList<TreeNode> result=new ArrayList<TreeNode>();
+        if(start>end){
+            result.add(null);
+            return result;
+        }
+        for(int i=start;i<=end;++i){
+            for(TreeNode left:generateTrees(start,i-1)){
+                for(TreeNode right:generateTrees(i+1,end)){
+                    TreeNode root=new TreeNode(i);
+                    root.left=left;
+                    root.right=right;
+                    result.add(root);
+                }
+            }
+        }
+        return result;
+    }
+}
