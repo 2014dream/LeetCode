54a386f5a72095831fdfa43d74bd52fa848489bc
2014dream <shaosong2014@gmail.com>
2014-05-12 22:40:52 -0600

    Update Longest Consecutive Sequence.java

--- Longest Consecutive Sequence.java
+++ Longest Consecutive Sequence.java
@@ -6,7 +6,7 @@ The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 
 Your algorithm should run in O(n) complexity.*/
 
-/* Use a hashset to store all the elements, traverse the array to find the left and right element.*.
+/* Use a hashset to store all the elements, traverse the array to find the left and right element.*／
 
 public class Solution {
     public int longestConsecutive(int[] num) {
