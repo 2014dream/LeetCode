18b2533c0364a32eb16060a98471604411d912ba
2014dream <shaosong2014@gmail.com>
2014-10-24 15:26:39 -0600

    Update Decode Ways.java

--- Decode Ways.java
+++ Decode Ways.java
@@ -12,7 +12,7 @@ Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
 The number of ways decoding "12" is 2.*/
 
 /* DP problem. let dp[i] be the number of decode ways to the length of i. if s[i-1] is not '0', then dp[i]=dp[i-1].
-if the combination of s[i-2] and s[i-1] is valid, means s[i-2] is not '0' and 1<=num<=26, then dp[i]=dp[i-1]+dp[i-2]. */
+if the combination of s[i-2] and s[i-1] is valid, means s[i-2] is not '0' and 11<=num<=26, then dp[i]=dp[i-1]+dp[i-2]. */
 
 
 public class Solution {
